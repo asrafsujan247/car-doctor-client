5e63c0abe3e47f2be5714bf150e47371c273357d
asrafsujan247 <asraf.sujan247@gmail.com>
2023-06-03 23:50:02 +0600

    added some information in about section

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -3,20 +3,33 @@ import parts from "../../../assets/images/about_us/parts.jpg";
 
 const About = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
-        <div className="lg:w-1/2">
+        <div className="lg:w-1/2 relative">
           <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
-          <img src={parts} className="max-w-sm rounded-lg shadow-2xl" />
+          <img
+            src={parts}
+            className="w-1/2 absolute right-6 top-1/2 border-8 border-white rounded-lg shadow-2xl"
+          />
         </div>
-        <div className="lg:w-1/2">
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div className="lg:w-1/2 space-y-6 p-5">
+          <h3 className="text-3xl text-orange-600 font-bold">About Us</h3>
+          <h1 className="text-5xl font-bold">
+            We are qualified & of experience in this field
+          </h1>
+          <p className="text-gray-500">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomized words which do not look even slightly
+            believable.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <p className="text-gray-500">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomized words which do not look even slightly
+            believable.
+          </p>
+          <button className="btn btn-warning">Get More Info</button>
         </div>
       </div>
     </div>
