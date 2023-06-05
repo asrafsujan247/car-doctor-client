b3da42e5e0ad192fd68ede5ddafe94064cbada6f
asrafsujan247 <asraf.sujan247@gmail.com>
2023-06-05 16:19:54 +0600

    added sign up page and gave some styles

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,73 @@
+import { Link } from "react-router-dom";
+import signupImg from "../../assets/images/login/login.svg";
+
+const SignUp = () => {
+  const handleSignUp = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="w-1/2 mr-12">
+          <img src={signupImg} alt="" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm border-2 border-gray-200 p-6">
+          <div className="card-body">
+            <h1 className="text-3xl font-bold text-center">Sign Up</h1>
+            <form onSubmit={handleSignUp}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Confirm Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Sign Up"
+                  className="btn hover:text-orange-600 bg-orange-600 border-none text-white font-semibold"
+                />
+              </div>
+            </form>
+            <p className="text-center my-4">
+              Already have an account?{" "}
+              <Link to="/login" className="text-orange-600 font-bold">
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
