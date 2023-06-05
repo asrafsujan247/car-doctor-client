f9ecde17ee581c877bb95f1f38b03c63198e8685
asrafsujan247 <asraf.sujan247@gmail.com>
2023-06-05 08:17:55 +0600

    added some changes in about section and added services section, service card, service data

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         <div className="lg:w-1/2 space-y-6 p-5">
           <h3 className="text-3xl text-orange-600 font-bold">About Us</h3>
           <h1 className="text-5xl font-bold">
-            We are qualified & of experience in this field
+            We are qualified <br /> & of experience <br /> in this field
           </h1>
           <p className="text-gray-500">
             There are many variations of passages of Lorem Ipsum available, but
