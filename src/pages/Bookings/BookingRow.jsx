2dc244950e6428f0751e002a46da0b4eb42b738b
asrafsujan247 <asraf.sujan247@gmail.com>
2023-06-07 21:00:29 +0600

    updated booking data when it is deleted

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,37 +1,5 @@
-import Swal from "sweetalert2";
-
-const BookingRow = ({ booking }) => {
+const BookingRow = ({ booking, handleDelete }) => {
   const { _id, price, img, date, service } = booking;
-
-  const handleDelete = (id) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        fetch(`http://localhost:5000/bookings/${id}`, {
-          method: "DELETE",
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            if (data.deletedCount > 0) {
-              Swal.fire(
-                "Deleted!",
-                "Your Booking has been deleted.",
-                "success"
-              );
-            }
-          })
-          .catch((error) => console.log(error));
-      }
-    });
-  };
 
   return (
     <tr>
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import Swal from "sweetalert2";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
@@ -16,6 +17,39 @@ const Bookings = () => {
       })
       .catch((error) => console.log(error));
   }, []);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire(
+                "Deleted!",
+                "Your Booking has been deleted.",
+                "success"
+              );
+              const remaining = bookings.filter(
+                (booking) => booking._id !== id
+              );
+              setBookings(remaining);
+            }
+          })
+          .catch((error) => console.log(error));
+      }
+    });
+  };
 
   return (
     <div>
@@ -35,7 +69,11 @@ const Bookings = () => {
           </thead>
           <tbody>
             {bookings.map((booking) => (
-              <BookingRow key={booking._id} booking={booking}></BookingRow>
+              <BookingRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+              ></BookingRow>
             ))}
           </tbody>
         </table>
