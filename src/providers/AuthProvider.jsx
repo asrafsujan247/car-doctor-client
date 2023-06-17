165561531c98245964083daf294d13106899609a
asrafsujan247 <asraf.sujan247@gmail.com>
2023-06-17 21:17:32 +0600

    changed some jwt codes in auth provider and loging and deployed app in firebase

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -49,7 +49,7 @@ const AuthProvider = ({ children }) => {
           email: currentUser.email,
         };
 
-        fetch("http://localhost:5000/jwt", {
+        fetch("https://car-doctor-server-jade-nine.vercel.app/jwt", {
           method: "POST",
           headers: {
             "content-type": "application/json",
